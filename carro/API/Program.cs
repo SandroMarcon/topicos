d09d0193e392505af2301bea9654d360a0e5ed3a
SANDRO JOAO DE LARIA MARCON <s.marcon@cs.up.edu.br>
2025-03-27 11:28:07 -0300

    aula 27/03/2025

--- carro/API/Program.cs
+++ carro/API/Program.cs
@@ -1,17 +1,68 @@
-using API.models;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
-List<Carro> carros = [
-    new Carro() {Id = 1, Name = "Fusca"},
-    new Carro() {Id = 2, Name = "Agera"},
-    new Carro() {Id = 3, Name = "R34 puple edition"}
- ];
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/api/carros",() => {
-    return Results.Ok(carros);
+
+app.MapGet("/api/carros", ([FromServices] AppDataContext ctx) => {
+
+    if (ctx.Carros.Any()) {
+        return Results.Ok(ctx.Carros.ToList());
+    }
+
+    return Results.NotFound();
+
 });
+
+app.MapPost("/api/carros", ([FromBody] Carro carro,
+                            [FromServices] AppDataContext ctx) => {
+
+        ctx.Carros.Add(carro);
+        ctx.SaveChanges();
+
+        return Results.Created("", carro);
+});
+
+app.MapGet("/api/carros/{id}", ([FromRoute] int id,
+                [FromServices] AppDataContext ctx) => {
+    Carro? carro = ctx.Carros.Find(id);
+
+    if(carro != null){
+        return Results.Ok(carro);
+    }
+    return Results.NotFound();
+});
+
+app.MapPut("/api/carros/{id}", ([FromRoute] int id,
+                [FromBody] Carro carro,
+                [FromServices] AppDataContext ctx) =>{
+    
+    Carro? etidade = ctx.Carros.Find(id);
+
+    if(etidade != null){
+        etidade.Name = carro.Name;
+        ctx.Carros.Update(etidade);
+        ctx.SaveChanges();
+        return Results.Ok(etidade);
+    }
+    return Results.NotFound();
+
+});
+
+app.MapDelete("/api/carros/{id}", ([FromRoute] int id,
+                [FromServices] AppDataContext ctx) =>{
+
+    Carro? carro = ctx.Carros.Find(id);
+    if(carro == null){
+        return Results.NotFound();
+    }
+    ctx.Carros.Remove(carro);
+    ctx.SaveChanges();
+    return Results.NoContent();
+});
+
 
 app.Run();
--- carro/API/models/Carro.cs
+++ carro/API/models/Carro.cs
@@ -1,4 +1,4 @@
-namespace API.models
+namespace API.Models
 {
     public class Carro
     {
